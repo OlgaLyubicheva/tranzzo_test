2ab49bdba54cffaa44e42a760d70cdad951f422a
Olga Lyubicheva <forjobslov@gmail.com>
2019-11-30 12:59:53 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const App = () => {
+  return (
+    <div></div>
+  )
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: "index.html"
+      }
+    )
+  ]
+};
